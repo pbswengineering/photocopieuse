f52ce4d6f2cc8e76047bcd60a72bac2ce1612fb1
Paolo Bernardi <paolo@bernardi.cloud>
2024-03-28 18:37:45 +0100

    VirtualEnv support

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+cd -P -- "$(dirname -- "$0")"
 ANACONDA="$HOME/anaconda3/bin/python"
 if [ -f "$ANACONDA" ]; then
     PYTHON="$ANACONDA"
+elif [ -f venv/bin/python ]; then
+    PYTHON=venv/bin/python
 else
     PYTHON='python3'
 fi
-cd -P -- "$(dirname -- "$0")"
 PYTHONPATH=src/main/python "$PYTHON" src/main/python/main.py "$@"
