2b3ca58002a58e956aeca8f194db1e7b4e3990c3
Paolo Bernardi <paolo@bernardi.cloud>
2025-06-23 16:25:54 +0200

    virtualenvs directory change

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+ENVSDIR="$HOME/.virtualenvs"
+
 cd -P -- "$(dirname -- "$0")"
 # Meant to work with a standard virtualenv-wrapper setup
-if [ -f "$HOME/Envs/photocopieuse/bin/python" ]; then
-    PYTHON="$HOME/Envs/photocopieuse/bin/python"
+if [ -f "$ENVSDIR/photocopieuse/bin/python" ]; then
+    PYTHON="$ENVSDIR/photocopieuse/bin/python"
 elif [ -f venv/bin/python ]; then
     PYTHON=venv/bin/python
 else
